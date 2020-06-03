1a25e42631c9f4a168965810ccb109c5db3d5937
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-06-03 21:20:33 +0100

    updated login to use username instead of email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
+    user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password is invalid"
+      flash.now[:alert] = "Username or password is invalid"
       render "new"
     end
   end
@@ -18,4 +18,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end
-end  
+end
