933821495865e26314b749fde4dd4d5b99770daa
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-06-04 16:22:32 +0100

    user can add new post only when logged in

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -9,27 +9,27 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content("Hello, world!")
   end
 
-  # scenario "Can only submit a post if user is logged in" do
-  #   visit "/"
-  #   expect(page).to have_no_link('New post')
-  #
-  #   click_link('Sign Up')
-  #
-  #   fill_in('Email', with: 'fatcontroller@example.com')
-  #   fill_in('Username', with: 'FatController')
-  #   fill_in('Name', with: 'Fat')
-  #   fill_in('Password', with: 'password123')
-  #   fill_in('Password confirmation', with: 'password123')
-  #   click_button('Create User')
-  #   click_link('Login')
-  #
-  #   fill_in('Username', with: 'FatController')
-  #   fill_in('Password', with: 'password123')
-  #   click_button('Login')
-  #
-  #   expect(page).to have_link('New post')
-  #
-  # end
+  scenario "Can only submit a post if user is logged in" do
+    visit "/"
+    expect(page).to have_no_link('New post')
+
+    click_link('Sign Up')
+
+    fill_in('Email', with: 'fatcontroller@example.com')
+    fill_in('Username', with: 'FatController')
+    fill_in('Name', with: 'Fat')
+    fill_in('Password', with: 'password123')
+    fill_in('Password confirmation', with: 'password123')
+    click_button('Create User')
+    click_link('Login')
+
+    fill_in('Username', with: 'FatController')
+    fill_in('Password', with: 'password123')
+    click_button('Login')
+
+    expect(page).to have_link('New post')
+
+  end
 
 
   # scenario "Can submit posts and view them" do
