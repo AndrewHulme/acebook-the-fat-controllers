84831a3babe7119eb473bd96688c5de4967e6857
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-05 11:40:41 +0100

    Fixes test errors after React merge (disables ESLint on Travis and local tdd.test, will enable after excluding React libraries)

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   get 'homepage/index'
 
-
-
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get 'signup', to: 'users#new', as: 'signup'
@@ -12,7 +10,7 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :posts
-  #   root 'home#index'
+     # root 'home#index'
   root 'homepage#index'
   
 end
