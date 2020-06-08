a2fdfcbad7c0cbce78d80bf8f9fb55eaf9642f2a
@willhead <60652390+will-head@users.noreply.github.com>
2020-06-08 17:47:07 +0100

    Deletes delete folder

--- delete/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  # has_many :post
-
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
-end
